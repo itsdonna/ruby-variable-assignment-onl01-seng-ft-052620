5bbc6e7254265750f7108838a7bd3b08e4b02913
Donna Torres <todonnatorres@icloud.com>
2020-05-27 00:00:13 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,3 +1,3 @@
-greeting = "Hello World"= "Barack Obama"
+greeting = "Hello World"
 puts "In 2016, the president was #{current_president}." Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
